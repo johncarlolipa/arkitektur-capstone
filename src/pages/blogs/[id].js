b410f60493f67a7503348183c27c12f946cb4454
unknown <johncarlolipa@gmail.com>
2023-09-20 08:04:39 +0800

    fix id and index blogs

--- src/pages/blogs/[id].js
+++ src/pages/blogs/[id].js
@@ -1,4 +1,6 @@
 import React from "react";
+import parse from "html-react-parser";
+import Image from "next/image";
 
 export const getStaticPaths = async () => {
   try {
@@ -53,11 +55,87 @@ export const getStaticProps = async (context) => {
 };
 
 export default function Details({ post }) {
+  const addSpacesBeforeH4AndParagraphs = (content) => {
+    content = content.replace(/<h4/g, "<p>&nbsp;</p><h4");
+    content = content.replace(/<p>/g, "<p>&nbsp;</p>");
+
+    return content;
+  };
+
+  const formattedContent = addSpacesBeforeH4AndParagraphs(
+    post.content.rendered
+  );
   return (
-    <div>
-      <h1>Details of the Blog</h1>
-      <p>{post.title.rendered}</p>
-      <p> {post.x_author}</p>
+    <div className="bg-white pb-24">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            From the blog
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+          <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+            <div
+              key={post.id}
+              className="relative isolate flex flex-col gap-8 lg:flex-row"
+            >
+              <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                <Image
+                  src={post.x_featured_media}
+                  alt={post.title.rendered}
+                  width={0}
+                  height={0}
+                  className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+              </div>
+              <div>
+                <div className="group relative max-w-xl">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href="#">
+                      <span className="absolute inset-0" />
+                      {post.title.rendered}
+                    </a>
+                  </h3>
+                  <div className="flex items-center gap-x-4 text-xs mt-4">
+                    <div className="relative flex items-center gap-x-4">
+                      <Image
+                        src={post.x_featured_media}
+                        alt={post.title.rendered}
+                        width={0}
+                        height={0}
+                        className="h-10 w-10 rounded-full bg-gray-50"
+                      />
+                      <div className="text-sm leading-6">
+                        <p className="font-semibold text-gray-900">
+                          <a href="#">
+                            <span className="absolute inset-0" />
+                            {post.x_author}
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                    <time dateTime={post.datetime} className="text-gray-500">
+                      {new Date(post.date).toDateString()}
+                    </time>
+                    <a
+                      href="#"
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                    >
+                      {post.categories}
+                    </a>
+                  </div>
+                  <div className="mt-5 text-sm leading-6 text-gray-600">
+                    {parse(formattedContent)}
+                  </div>
+                </div>
+                <div className="mt-6 flex border-t border-gray-900/5 pt-6"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import React from "react";
-import parse from "html-react-parser";
 import Image from "next/image";
+import { Playfair_Display } from "next/font/google";
+
+const playfair_display = Playfair_Display({ subsets: ["latin"] });
+
 export const getStaticProps = async () => {
   const res = await fetch(
     "https://dev-arkitektur-headless.pantheonsite.io//wp-json/wp/v2/posts"
@@ -15,15 +18,67 @@ export const getStaticProps = async () => {
 
 export default function blogs({ blogs }) {
   return (
-    <div>
-      <h1>All Blogs</h1>
-      {blogs.map((post) => (
-        <div key={post.id}>
-          <Link href={`/blogs/${post.id}`} key={post.id}>
-            <h3>{post.title.rendered}</h3>
-          </Link>
+    <div className="bg-white pb-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="text-center">
+          <p className="mb-4 text-md leading-8 text-[#A4A4A4]">From the Blog</p>
+          <h2
+            className={`${playfair_display.className} mb-12 text-3xl font-bold text-[#333333] sm:text-4xl`}
+            style={{ lineHeight: "1.5" }}
+          >
+            Exploring Design and Inspiring Visions
+          </h2>
         </div>
-      ))}
+
+        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {blogs.map((post) => (
+            <article
+              key={post.id}
+              className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+            >
+              <Image
+                src={post.x_featured_media}
+                alt={post.title.rendered}
+                width={0}
+                height={0}
+                className="absolute inset-0 -z-10 h-full w-full object-cover"
+              />
+              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+              <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+                <time dateTime={post.date} className="mr-8">
+                  {new Date(post.date).toDateString()}
+                </time>
+                <div className="-ml-4 flex items-center gap-x-4">
+                  <svg
+                    viewBox="0 0 2 2"
+                    className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50"
+                  >
+                    <circle cx={1} cy={1} r={1} />
+                  </svg>
+                  <div className="flex gap-x-2.5">
+                    <Image
+                      src={post.x_featured_media}
+                      alt={post.title.rendered}
+                      width={0}
+                      height={0}
+                      className="h-6 w-6 flex-none rounded-full bg-white/10"
+                    />
+                    {post.x_author}
+                  </div>
+                </div>
+              </div>
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
+                <Link href={`/blogs/${post.id}`} key={post.id}>
+                  <span className="absolute inset-0" />
+                  {post.title.rendered}
+                </Link>
+              </h3>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
